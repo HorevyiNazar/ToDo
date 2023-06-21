be1e93aa9c8e9f4b5c30d3128010eee20fdab891
HorevyiNazar <goreviynazar@gmail.com>
2023-06-21 15:20:34 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Назар Горевой on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
